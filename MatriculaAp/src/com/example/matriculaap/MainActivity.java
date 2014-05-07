3f0dd94dec4bc242406e82905e0137240d40da71
LC <LC@LC2-PC05>
2014-05-07 11:18:47 -0500

    njbnjkbik

--- MatriculaAp/src/com/example/matriculaap/MainActivity.java
+++ MatriculaAp/src/com/example/matriculaap/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.matriculaap;
 
-import java.security.Principal;
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 

d0e252797c475764dfb82fe197cdba8ed4e6a005
JhOnShiN <JhOnShiN@JhOnShiN-PC>
2014-06-25 00:04:45 -0500

    Creo modelos para extraer datos del api @JhonCcaypani

---
+++ MatriculaAp/src/com/example/model/ModelMain.java
@@ -0,0 +1,36 @@
+package com.example.model;
+
+public class ModelMain {
+	
+	private String totalMatric;
+	private String matricPrimVez;
+	private String matricVirtual;
+	private String matricPresen;
+	
+	
+	public String getTotalMatric() {
+		return totalMatric;
+	}
+	public void setTotalMatric(String totalMatric) {
+		this.totalMatric = totalMatric;
+	}
+	public String getMatricPrimVez() {
+		return matricPrimVez;
+	}
+	public void setMatricPrimVez(String matricPrimVez) {
+		this.matricPrimVez = matricPrimVez;
+	}
+	public String getMatricVirtual() {
+		return matricVirtual;
+	}
+	public void setMatricVirtual(String matricVirtual) {
+		this.matricVirtual = matricVirtual;
+	}
+	public String getMatricPresen() {
+		return matricPresen;
+	}
+	public void setMatricPresen(String matricPresen) {
+		this.matricPresen = matricPresen;
+	}
+
+}
---
+++ MatriculaAp/src/com/example/model/ModelProceso.java
@@ -0,0 +1,37 @@
+package com.example.model;
+
+public class ModelProceso {
+	
+	private String virtualProceso;
+	private String virtualConfirm;
+	private String precensialProceso;
+	private String presencialConfirm;
+	
+	
+	public String getVirtualProceso() {
+		return virtualProceso;
+	}
+	public void setVirtualProceso(String virtualProceso) {
+		this.virtualProceso = virtualProceso;
+	}
+	public String getVirtualConfirm() {
+		return virtualConfirm;
+	}
+	public void setVirtualConfirm(String virtualConfirm) {
+		this.virtualConfirm = virtualConfirm;
+	}
+	public String getPrecensialProceso() {
+		return precensialProceso;
+	}
+	public void setPrecensialProceso(String precensialProceso) {
+		this.precensialProceso = precensialProceso;
+	}
+	public String getPresencialConfirm() {
+		return presencialConfirm;
+	}
+	public void setPresencialConfirm(String presencialConfirm) {
+		this.presencialConfirm = presencialConfirm;
+	}
+
+	
+}

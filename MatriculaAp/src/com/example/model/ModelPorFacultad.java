886ec7405aa42706e9c1edf4d6d06a9c9e7adc21
JhOnShiN <JhOnShiN@JhOnShiN-PC>
2014-06-25 00:04:45 -0500

    nadaaa

---
+++ MatriculaAp/src/com/example/model/ModelPorFacultad.java
@@ -0,0 +1,37 @@
+package com.example.model;
+
+
+public class ModelPorFacultad {
+
+	private String salud;
+	private String empresariales;
+	private String educacion;
+	private String ingenieria;
+	
+	
+	public String getSalud() {
+		return salud;
+	}
+	public void setSalud(String salud) {
+		this.salud = salud;
+	}
+	public String getEmpresariales() {
+		return empresariales;
+	}
+	public void setEmpresariales(String empresariales) {
+		this.empresariales = empresariales;
+	}
+	public String getEducacion() {
+		return educacion;
+	}
+	public void setEducacion(String educacion) {
+		this.educacion = educacion;
+	}
+	public String getIngenieria() {
+		return ingenieria;
+	}
+	public void setIngenieria(String ingenieria) {
+		this.ingenieria = ingenieria;
+	}
+	
+}
---
+++ MatriculaAp/src/com/example/model/ModelPorVivienda.java
@@ -0,0 +1,22 @@
+package com.example.model;
+
+public class ModelPorVivienda {
+	
+	private String interno;
+	private String externo;
+	
+	
+	public String getInterno() {
+		return interno;
+	}
+	public void setInterno(String interno) {
+		this.interno = interno;
+	}
+	public String getExterno() {
+		return externo;
+	}
+	public void setExterno(String externo) {
+		this.externo = externo;
+	}
+	
+}
